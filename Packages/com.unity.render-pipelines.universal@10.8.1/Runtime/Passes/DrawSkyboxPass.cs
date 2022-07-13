4890e8d23beadb483fa53fb3acaf44116b228cbe
xhh <410830616@qq.com>
2022-07-13 15:37:01 +0800

    URP 移除2D 移除VR相关代码

---
+++ Packages/com.unity.render-pipelines.universal@10.8.1/Runtime/Passes/DrawSkyboxPass.cs
@@ -0,0 +1,26 @@
+namespace UnityEngine.Rendering.Universal
+{
+    /// <summary>
+    /// Draw the skybox into the given color buffer using the given depth buffer for depth testing.
+    ///
+    /// This pass renders the standard Unity skybox.
+    /// </summary>
+    public class DrawSkyboxPass : ScriptableRenderPass
+    {
+        public DrawSkyboxPass(RenderPassEvent evt)
+        {
+            base.profilingSampler = new ProfilingSampler(nameof(DrawSkyboxPass));
+
+            renderPassEvent = evt;
+        }
+
+        /// <inheritdoc/>
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+
+            {
+                context.DrawSkybox(renderingData.cameraData.camera);
+            }
+        }
+    }
+}
